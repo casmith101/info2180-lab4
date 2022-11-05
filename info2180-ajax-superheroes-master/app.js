bd7118869f9664910f42e2d94103a347f45af70b
Carrissa Smith <smithcarrissa1@gmail.com>
2022-11-05 16:58:07 -0700

    Edited JS File

--- info2180-ajax-superheroes-master/app.js
+++ info2180-ajax-superheroes-master/app.js
@@ -1,5 +1,6 @@
 window.onload= function(){
     let button= this.document.querySelector('#btn')
+    let search= document.querySelector('#search')
 
     button.addEventListener("click", function(element){
         element.preventDefault();
@@ -13,9 +14,16 @@ window.onload= function(){
                 }
             })
             .then(data => {
-                let list = document.querySelector('#list');
-                list.innerHTML = data;
-                alert(data);
+                let result = document.querySelector('#result');
+                result.innerHTML = data;
+                console.log(data);
             })
     })
+
+    /*$.ajax('http://localhost/info2180-lab4/info2180-ajax-superheroes-master/superheroes.php', {
+        method: 'GET'
+    }).done(function(response) {
+        result.text(response);
+        result.innerHTML
+    })*/
 }
